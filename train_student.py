d85ed5e35404cef695ff692c780765313f126fb4
david-0429 <86578317+david-0429@users.noreply.github.com>
2022-11-09 19:31:14 +0900

    Update train_student.py

--- train_student.py
+++ train_student.py
@@ -83,9 +83,9 @@ def parse_option():
                                                                       'rkd', 'pkt', 'abound', 'factor', 'nst'])
     parser.add_argument('--trial', type=str, default='1', help='trial id')
 
-    parser.add_argument('-r', '--gamma', type=float, default=1, help='weight for classification')
+    parser.add_argument('-r', '--gamma', type=float, default=1, help='weight for CrossEntropyLoss')
     parser.add_argument('-a', '--alpha', type=float, default=None, help='weight balance for KD')
-    parser.add_argument('-b', '--beta', type=float, default=None, help='weight balance for other losses')
+    parser.add_argument('-b', '--beta', type=float, default=None, help='weight balance for other losses(not for base KD)')
 
     # KL distillation
     parser.add_argument('--kd_T', type=float, default=4, help='temperature for KD distillation')
